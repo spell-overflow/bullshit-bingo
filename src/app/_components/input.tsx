e20301a169964ab6424a1568d5b6b2bc77d7bced
Anneke Hugenberg <annekehugenberg@proton.me>
2024-09-06 02:32:32 +0200

    fix Input Component to work on keyevent "enter"

--- src/app/_components/input.tsx
+++ src/app/_components/input.tsx
@@ -15,6 +15,7 @@ const Input: React.FC<inputProperties> = ({
   placeholder = "placeholder",
   value,
   onChange,
+  onKeyDown,
 }) => {
   return (
     <div>
@@ -29,6 +30,7 @@ const Input: React.FC<inputProperties> = ({
         placeholder={placeholder}
         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         onChange={onChange}
+        onKeyDown={onKeyDown}
       />
     </div>
   );
