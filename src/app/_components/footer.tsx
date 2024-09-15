0d7efa4fafc4dcc179a77cbdefac03bd82c9a8b4
Anneke Hugenberg <annekehugenberg@proton.me>
2024-09-15 22:29:51 +0200

    add about page; closes #33

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -5,6 +5,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="mx-4 my-4 mt-auto flex h-fit justify-center gap-5 rounded-t-lg bg-accent py-4">
       <Link href="/impressum">Impressum</Link>
+      <Link href="/about">About Bullshit-Bingo</Link>
     </footer>
   );
 };
---
+++ src/app/about/page.tsx
@@ -0,0 +1,100 @@
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import Lettering from "../_components/lettering";
+import Bubble from "../_components/bubble";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "~/components/ui/accordion";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="gap-4">
+        <div className="w-full text-center">
+          <div className="inline-block align-middle">
+            <Lettering />
+          </div>
+          <div className="inline-block align-middle">
+            <Bubble />
+          </div>
+        </div>
+      </div>
+      <Link href={"/"} className="flex justify-center">
+        <Button>Return to Bingo</Button>
+      </Link>
+      <div className="m-4 mx-auto max-w-3xl rounded-xl bg-accent p-4 text-accent-foreground sm:px-6 lg:max-w-5xl lg:px-8">
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>What is the Bullshit Bingo app?</AccordionTrigger>
+            <AccordionContent>
+              The Bullshit Bingo app is an entertaining game that is
+              characterized by everyday, unavoidable tasks or annoying events.
+              was inspired. Instead of getting annoyed about these things, you
+              can them in a bingo board and playfully manage or tick them off.
+              or tick them off.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>
+              What happens if I have fewer than 25 terms?
+            </AccordionTrigger>
+            <AccordionContent>
+              You can add your own terms or tasks using the input field. Simply
+              enter the desired text and add it to your list.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>
+              Will my entries and the playing field be saved?
+            </AccordionTrigger>
+            <AccordionContent>
+              Currently, the app only saves the list of entries, but not the
+              field and the score.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4">
+            <AccordionTrigger>
+              Who developed the Bullshit Bingo app?
+            </AccordionTrigger>
+            <AccordionContent>
+              The Bullshit Bingo app is a project by Anneke Hugenberg. The app
+              was created as a training project for web development.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-5">
+            <AccordionTrigger>
+              Where did the idea for the Bullshit Bingo app come from?
+            </AccordionTrigger>
+            <AccordionContent>
+              {`In the winter of 2021, I was working in the orthopaedic department
+              of a hospital. It was Monday morning, 2 colleagues were sick, I
+              had overslept and didn't have time for a coffee before I left the
+              house and I was sitting in the doctor's room with my favorite
+              colleague preparing discharges and the upcoming ward round when a
+              nurse came in and told us that the patient who was to be taken to
+              the operating room in 5 minutes was munching on a bread roll. He
+              was therefore not sober and could not be operated on. For us and
+              our colleagues, this meant a mountain of additional work and
+              organization (changes to the operating schedule, who would be the
+              first to be operated on instead? Is he already there and
+              prepared...). Something like this happened from time to time,
+              although everyone involved actually knows that patients are not
+              allowed to eat on the day of surgery... There are probably things
+              like that in every job. Things that you just can't get rid of
+              completely, that keep happening again and again, they get on your
+              nerves but there's nothing you can do other than to reschedule and
+              get as annoyed as possible. That's how the idea of playing
+              bullshit bingo with colleagues was born. And to write these things
+              in the boxes. So at least there was something positive about it
+              that we could be happy about and which sweetened the working day a
+              little.`}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </div>
+  );
+}
