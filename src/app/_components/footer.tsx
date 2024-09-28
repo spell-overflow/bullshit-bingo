5b12a15c7f5d3811aaeb228a807bdb98df53e2a4
Anneke Hugenberg <spelloverflow@proton.me>
2024-09-28 14:13:16 +0200

    add underline on hover to footer; closes #47

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -4,9 +4,15 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <footer className="mx-4 mt-auto flex h-fit justify-center gap-5 rounded-t-lg bg-accent py-4">
-      <Link href="/about">About Bullshit-Bingo</Link>
-      <Link href="/impressum">Impressum</Link>
-      <Link href="/datenschutz">Datenschutz</Link>
+      <Link href="/about" className="hover:underline">
+        About Bullshit-Bingo
+      </Link>
+      <Link href="/impressum" className="hover:underline">
+        Impressum
+      </Link>
+      <Link href="/datenschutz" className="hover:underline">
+        Datenschutz
+      </Link>
     </footer>
   );
 };
