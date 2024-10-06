d80ebd146c988e513634c9e57a4dc3085ff1c781
Anneke Hugenberg <annekehugenberg@proton.me>
2024-10-06 02:52:34 +0200

    add login and logout-logic to user-page; closes #63

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,5 +1,38 @@
-import LoginCard from "../_components/loginCard";
+"use client";
+
+import { faSignInAlt, faSignOutAlt } from "@fortawesome/pro-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useSession, signOut, signIn } from "next-auth/react";
+import { Button } from "~/components/ui/button";
 
 export default function User(): JSX.Element {
-  return <LoginCard />;
+  const { data: session } = useSession();
+  const userName = session ? session?.user.name : "You wonderful human being";
+  const greeting = `Hello ${userName}!💗`;
+  return (
+    <div className="flex flex-col items-center text-center text-lg">
+      <div>{greeting}</div>
+      {session ? (
+        <Button
+          onClick={async () => {
+            await signOut();
+          }}
+          className="mt-4"
+        >
+          <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
+          Logout
+        </Button>
+      ) : (
+        <Button
+          onClick={async () => {
+            await signIn();
+          }}
+          className="mt-4"
+        >
+          <FontAwesomeIcon icon={faSignInAlt} className="mr-2" />
+          Login
+        </Button>
+      )}
+    </div>
+  );
 }
