9516438cd8f2bafe8134373192908c32102f1c34
Anneke Hugenberg <annekehugenberg@proton.me>
2024-10-08 02:54:59 +0200

    prevent cursorchange on playfieldentries; closes #8

--- src/app/playfield/page.tsx
+++ src/app/playfield/page.tsx
@@ -168,7 +168,7 @@ function PlayfieldElement({
 }): JSX.Element {
   return (
     <div
-      className="relative flex aspect-square min-h-12 items-center justify-center text-ellipsis rounded-sm bg-primary text-center text-primary-foreground shadow sm:max-h-24"
+      className="relative flex aspect-square min-h-12 cursor-default items-center justify-center text-ellipsis rounded-sm bg-primary text-center text-primary-foreground shadow sm:max-h-24"
       onClick={() => {
         onChange(!entry.crossed);
       }}
