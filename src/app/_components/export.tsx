5657a564704a0fb41dcf87650914c2a313029f41
Anneke Hugenberg <annekehugenberg@proton.me>
2024-10-17 20:40:18 +0200

    add logos to exported docx-file

--- src/app/_components/export.tsx
+++ src/app/_components/export.tsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpFromBracket } from "@fortawesome/pro-regular-svg-icons";
 import {
   Document,
+  ImageRun,
   Packer,
   Paragraph,
   Table,
@@ -20,43 +21,58 @@ export default function Export({ className }: ExportProperties) {
   const playfield = importedPlayfield.data;
   const playfieldLength = playfield?.length ?? 0;
   const numberOfColumns = Math.sqrt(playfieldLength);
+  const playfieldId = playfield?.[0]?.playfield.id;
 
   async function exportToWord() {
     if (!playfield) {
       return;
     }
 
-    const table = new Table({
-      rows: Array.from(
-        { length: numberOfColumns },
-        (_, i) =>
-          new TableRow({
-            children: Array.from(
-              { length: numberOfColumns },
-              (_, j) =>
-                new TableCell({
-                  children: [
-                    new Paragraph({
-                      children: [
-                        new TextRun(
-                          String(
-                            playfield[i * numberOfColumns + j]?.playfieldentry
-                              ?.text ?? "",
-                          ),
-                        ),
-                      ],
-                    }),
-                  ],
-                }),
-            ),
+    const title = new Paragraph({
+      alignment: "center",
+      children: [
+        new ImageRun({
+          data: await fetch("/fulllogo.png").then((res) => res.arrayBuffer()),
+          transformation: { width: 100, height: 100 },
+          type: "png",
+        }),
+      ],
+    });
+
+    function createCell(text: string) {
+      return new TableCell({
+        children: [
+          new Paragraph({
+            children: [new TextRun(text)],
           }),
+        ],
+      });
+    }
+
+    function createRow(cells: TableCell[]) {
+      return new TableRow({ children: cells });
+    }
+
+    const table = new Table({
+      rows: Array.from({ length: numberOfColumns }, (_, i) =>
+        createRow(
+          Array.from({ length: numberOfColumns }, (_, j) =>
+            createCell(
+              playfield[i * numberOfColumns + j]?.playfieldentry?.text ?? "",
+            ),
+          ),
+        ),
       ),
+    });
+
+    const description = new Paragraph({
+      children: [new TextRun(`Bullshit Bingo Playfield: ${playfieldId}`)],
     });
 
     const doc = new Document({
       sections: [
         {
-          children: [table],
+          children: [title, description, table],
         },
       ],
     });
@@ -67,7 +83,7 @@ export default function Export({ className }: ExportProperties) {
     });
     const link = document.createElement("a");
     link.href = window.URL.createObjectURL(blob);
-    link.download = "my.docx";
+    link.download = `BullshitBingo Playfield: ${playfieldId}.docx`;
     link.click();
   }
   return (
